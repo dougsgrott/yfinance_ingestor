0ed715ba0d384c562a88f3c3715aebc28c8d2d7a
neovelleitas <doug.sgrott@gmail.com>
2022-03-04 20:45:56 -0300

    Refactored read_datafile function

--- src/data_curation.py
+++ src/data_curation.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import datetime
 from models import get_engine 
+import time
 
 #%%
 class TickerNotFoundOnIngestedDataException(Exception):
@@ -22,29 +23,35 @@ def get_files_from_layer(layer_path: str, ticker: str) -> list[str]:
     return tickers_full_paths
 
 
-def read_datafile(path):
-    if path.split('.')[-1] == 'csv':
-        return pd.read_csv(path, index_col=0)
-    elif path.split('.')[-1] == 'db':
-        database_url  = f"sqlite:///{path}"
-        table_name = path[:-3]
-        engine = get_engine(database_url)
-        return pd.read_sql_table(table_name, engine)
+def read_datafile(path_list, file_format) -> pd.DataFrame:
+    curated_df = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
+    if file_format == 'csv':
+        filtered_paths = [path for path in path_list  if path.split('.')[-1] == 'csv']
+        if len(filtered_paths) != 0:
+            curated_df = pd.read_csv(filtered_paths[-1], index_col=0)
+    
+    elif file_format == 'sqlite':
+        filtered_paths = [path for path in path_list  if path.split('.')[-1] == 'db']
+        if len(filtered_paths) != 0:
+            database_url  = f"sqlite:///{filtered_paths[-1]}"
+            table_name = filtered_paths[-1][:-3]
+            engine = get_engine(database_url)
+            curated_df = pd.read_sql_table(table_name, engine)
+    return curated_df
+
+        
 
 
-def curate_data_by_ticker(ingestion_path: str, ticker: str, curation_path: str) -> bool:
+def curate_data_by_ticker(ingestion_path: str, ticker: str, curation_path: str, file_format: str) -> bool:
         
     curated_data_paths = get_files_from_layer(layer_path=curation_path, ticker=ticker)
-    if curated_data_paths:
-        curated_df = read_datafile(curated_data_paths[-1])
-        # curated_df = pd.read_csv(curated_data_paths[-1], index_col=0)
-    else:
-        curated_df = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
+    curated_df = read_datafile(curated_data_paths, file_format)
 
     ingested_data_paths = get_files_from_layer(layer_path=ingestion_path, ticker=ticker)
     if ingested_data_paths:
         for path in ingested_data_paths:
                 df_aux = pd.read_csv(path)
+                # Merge / Upsert
                 curated_df = df_aux.set_index(['Date'])\
                                    .combine_first(curated_df.set_index(['Date']))\
                                    .reset_index()        
@@ -54,28 +61,37 @@ def curate_data_by_ticker(ingestion_path: str, ticker: str, curation_path: str) 
     return curated_df
 
 
-def save_curated_data(curation_path: str, ticker: str, curated_data: pd.DataFrame, output_format: str):
+def save_curated_data(curation_path: str, ticker: str, curated_data: pd.DataFrame, file_format: str):
     BASE_DIR = os.getcwd()
     curation_path = f"{BASE_DIR}/{curation_path}/{ticker}"
     curation_name = f"{datetime.datetime.now()}"
     if not os.path.exists(curation_path): 
         os.mkdir(curation_path) 
-    if output_format == 'csv':
+    if file_format == 'csv':
         curated_data.to_csv(f"{curation_path}/{curation_name}.csv")
-    if output_format == 'sqlite':
+    if file_format == 'sqlite':
         database_url = f"sqlite:////{curation_path}/{curation_name}.db"
         engine = get_engine(database_url)
-        curated_data.to_sql(f"{curation_path}/{curation_name}",
-                            con=engine,
-                            index=False,
-                            if_exists='replace')
+        name = f"{curation_path}/{curation_name}"
+        curated_data.to_sql(name, con=engine, index=False, if_exists='replace')
+    if file_format == 'postgres':
+        user = "root"
+        passwd = "root"
+        host = "localhost"
+        port = "5432"
+        db = "premise_postgres_db"
+        database_url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}" 
+        engine = get_engine(database_url)
+        name = ticker
+        curated_data.to_sql(ticker, con=engine, index=False, if_exists='replace')
 
 
-def curate_data_all_tickers(ingestion_path: str, curation_path: str, output_format: str) -> bool:
+
+def curate_data_all_tickers(ingestion_path: str, curation_path: str, file_format: str) -> bool:
     tickers_in_layer = os.listdir(ingestion_path)
     for ticker in tickers_in_layer:
-        curated_df = curate_data_by_ticker(ingestion_path=ingestion_path, ticker=ticker, curation_path=curation_path)
-        save_curated_data(curation_path=curation_path, ticker=ticker, curated_data=curated_df, output_format=output_format)
+        curated_df = curate_data_by_ticker(ingestion_path=ingestion_path, ticker=ticker, curation_path=curation_path, file_format=file_format)
+        save_curated_data(curation_path=curation_path, ticker=ticker, curated_data=curated_df, file_format=file_format)
 
     
 
@@ -83,15 +99,11 @@ def curate_data_all_tickers(ingestion_path: str, curation_path: str, output_form
 if __name__=="__main__":
     print('\n')
     # curate_data_by_ticker(ingestion_path="ingested_data", ticker="sopa", curation_path="curated_data")
-    curate_data_all_tickers(ingestion_path="ingested_data", curation_path="curated_data", output_format='sqlite')
-
-#%%
-
-# csv_data1 = pd.read_csv("/home/user/PythonProj/yfinance_ingestor/ingested_data/sopa/2022-03-01 17:04:24.566132.csv")
-# csv_data2 = pd.read_csv("/home/user/PythonProj/yfinance_ingestor/ingested_data/sopa/2022-03-01 19:18:23.959880.csv")
-
-
-#%%
-# csv_updated = csv_data2.set_index(['Date'])\
-#                        .combine_first(csv_data1.set_index(['Date']))\
-#                        .reset_index()
+    curate_data_all_tickers(ingestion_path="ingested_data", curation_path="curated_data", file_format='csv')
+    # time.sleep(2)
+    # curate_data_all_tickers(ingestion_path="ingested_data", curation_path="curated_data", file_format='csv')
+    # time.sleep(2)
+    # curate_data_all_tickers(ingestion_path="ingested_data", curation_path="curated_data", file_format='sqlite')
+    # time.sleep(2)
+    # curate_data_all_tickers(ingestion_path="ingested_data", curation_path="curated_data", file_format='sqlite')
+    # time.sleep(2)
